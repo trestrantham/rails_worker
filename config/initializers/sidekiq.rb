95b7ad8e3f3bafbb4d01c6a8f45fa0bc29be0fef
Tres Trantham <tres@trestrantham.com>
2014-09-10 00:34:21 -0400

    Update redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Figaro.env.redis_url, namespace: "worker" }
+  config.redis = { url: ENV["REDIS_PROVIDER"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Figaro.env.redis_url, namespace: "worker" }
+  config.redis = { url: ENV["REDIS_PROVIDER"] }
 end
